eae443f1174b3ae17a5985cca28072d909f2ca0a
barndalion <darellbarnes450@gmail.com>
2025-12-19 16:43:11 -0500

    solve user logger problem

--- authbarn/test.py
+++ authbarn/test.py
@@ -1,14 +1,14 @@
 from AuthBarn import *
 
 credential = ["127.0.0.1",3306,"root","Lionel12$","test"]
-test = Action(dev_mode=True, credentials=credential)
+test = Action(enable_logging=True,dev_mode=True, credentials=credential)
+test.log("Warning","error")
+# token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6ImRkIiwiUm9sZSI6IlVzZXIiLCJQZXJtaXNzaW9uIjpbXX0.KZpGBUxfOOp2LoNKlpmbeawtZxKeKzeNEt03CzIoXXk"
+# token_bytes = token.encode("utf-8")
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6ImRkIiwiUm9sZSI6IlVzZXIiLCJQZXJtaXNzaW9uIjpbXX0.KZpGBUxfOOp2LoNKlpmbeawtZxKeKzeNEt03CzIoXXk"
-token_bytes = token.encode("utf-8")
-
-@test.require_permission("Admin")
-def prinny(token):
-    print("hello, world!")
+# @test.require_permission("Admin")
+# def prinny(token):
+#     print("hello, world!")
 
 
-prinny(token_bytes)
+# prinny(token_bytes)
